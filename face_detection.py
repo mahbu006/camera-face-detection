17d6138221d32140d3b07063f4bd507573735401
Raiyan Mahbub <50161218+mahbu006@users.noreply.github.com>
2021-03-05 23:33:01 -0600

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,7 +1,7 @@
 import cv2
 import sys
 
-cascPath = sys.argv[1]
+cascPath = sys.argv[1] # Takes in file as its first argument
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -20,7 +20,7 @@ while True:
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
-    # Draw rectangle around face
+    # Draw rectangle around face on camera
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+w), (0, 255, 0), 2)
 
@@ -29,5 +29,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Break out of loop and quit
 video_capture.release()
 cv2.destroyAllWindows()
